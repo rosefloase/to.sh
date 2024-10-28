54a70eb0610edeecec1aa6b6145a51d99b3b0a5d
jade floase (thepersonever.net) <jadeharley0409@proton.me>
2024-10-28 17:56:11 -0500

    made logic to figure out what site is what, and what site to change

--- to.sh
+++ to.sh
@@ -1,3 +1,6 @@
 source tosh.conf
+source ${mainfolder}/.toInf
 
-echo $mainfolder
+echo "current ${mainfolder} will be moved to ${mainfolder}_${foldername}"
+
+echo "${mainfolder}_${1} will be moved to ${mainfolder}"
