a94c7b85f108c55c0388ffa1a459dfc3ebd75d43
jade floase (thepersonever.net) <jadeharley0409@proton.me>
2024-10-28 19:21:39 -0500

    added dummy function for naming, and started writing help

--- to.sh
+++ to.sh
@@ -1,13 +1,21 @@
 source tosh.conf
 source ${mainfolder}/.toInf
 
+
 arg=$1
 
 if [ -z "$arg" ]; then
-	printf "NO ARGUMENTS!!!! THIS.DIE \nif you want to get help, do \"to.sh -h\" or \"to.sh --help\" :D"
+	printf "NO ARGUMENTS!!!! THIS.DIE \nif you want to get help, do \"to.sh -h\" or \"to.sh --help\" :D \n"
+	exit
+elif [ $arg == "-n" ] || [ $arg == "--name" ]; then
+	printf "dummy test for naming argument"
 	exit
 elif [ $arg == "-h" ] || [ $arg == "--help" ]; then
-	printf "wow useful help message"
+	shh="\e[1;30m"
+	normal="\e[0m"
+
+	printf "${shh}psttt if you want to use to.sh as a regular command do \"alias to.sh='bash to.sh'\"${normal}\n"
+	printf "to.sh is a program designed to solve a very simple problem: Jade Harley doesn't know how to set up subdomains on apache servers!!! :("	
 	exit
 fi
 
