b450f498f9e1097899cc778832a6838dad5201bc
jade floase (thepersonever.net) <jadeharley0409@proton.me>
2024-10-28 20:12:41 -0500

    finished naming logic + help update

--- to.sh
+++ to.sh
@@ -1,13 +1,14 @@
 source tosh.conf
 arg=$1
+editor="vim"
 
 if [ -z "$arg" ]; then
 	printf "NO ARGUMENTS!!!! THIS.DIE \nif you want to get help, do \"to.sh -h\" or \"to.sh --help\" :D \n"
 	exit
 elif [ $arg == "-n" ] || [ $arg == "--name" ]; then
-	printf "dummy test for naming argument"
+	printf "naming folder..."
 	cp template.toInf $mainfolder/.toInf
-	vim $mainfolder/.toInf
+	$editor $mainfolder/.toInf
 	exit
 elif [ $arg == "-h" ] || [ $arg == "--help" ]; then
 	shh="\e[1;30m"
@@ -17,7 +18,7 @@ elif [ $arg == "-h" ] || [ $arg == "--help" ]; then
 	printf "to.sh is a program designed to solve a very simple problem: Jade Harley doesn't know how to set up subdomains on apache servers!!! :(\n"	
 	printf "to give a folder a to.shName, run \"to.sh [--name/-n] [to.shName]\"\n"
 	printf "to switch what folder is on main, run \"to.sh [to.shName]\"\n"
-	printf "to change your main folder's name, edit tosh.conf and update each folder... MANUALLY :(\n" 
+	printf "to change your main folder's name, or to change what text editor is used, edit tosh.conf! ${shh}if you update your main folder's name, you'll have to update each folder... MANUALLY :(${normal}\n" 
 	exit
 fi
 
